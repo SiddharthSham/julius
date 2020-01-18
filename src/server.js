3c9dd0ae23b1bb391c19fa8e4640e04202f8b050
siddharthsham <siddhsham@gmail.com>
2020-01-18 19:44:16 +0530

    switch from polka to express for server

--- src/server.js
+++ src/server.js
@@ -1,17 +1,31 @@
 import sirv from 'sirv';
-import polka from 'polka';
+import express from 'express';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
+function one(req, res, next) {
+	req.hello = 'world';
+	next();
+  }
+  
+  function two(req, res, next) {
+	req.foo = '...needs better demo 😔';
+	next();
+  }
+
+express() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
 	)
+	.get('/users/:id', (req, res) => {
+		console.log(`~> Hello, ${req.hello}`);
+		res.end(`User: ${req.params.id}`);
+	  })
 	.listen(PORT, err => {
 		if (err) console.log('error', err);
 	});
