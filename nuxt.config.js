416e9d6271b06475afcd4bbbdba3f0c5d1b09387
siddharthsham <siddhsham@gmail.com>
2020-01-19 13:47:58 +0530

    Switch to nuxtjs and nuxt-fire

---
+++ nuxt.config.js
@@ -0,0 +1,87 @@
+
+module.exports = {
+  mode: 'spa',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
+    '@nuxtjs/bulma',
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    [
+      'nuxt-fire',
+      {
+        config: {
+          apiKey: "AIzaSyClprWliE3UIWiJs31zw4jKNbNGPlbZJWI",
+          authDomain: "julius-e71be.firebaseapp.com",
+          databaseURL: "https://julius-e71be.firebaseio.com",
+          projectId: "julius-e71be",
+          storageBucket: "julius-e71be.appspot.com",
+          messagingSenderId: "816281274509",
+          appId: "1:816281274509:web:6ffbf36a4af19f37557ac4"
+        },
+        services: {
+          auth: true // Just as example. Can be any other service.
+        }
+      }
+    ]
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    postcss: {
+      preset: {
+        features: {
+          customProperties: false
+        }
+      }
+    },
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
