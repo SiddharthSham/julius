71c8dda68b9afe59af8ee31ff002ab889eb29531
siddharthsham <siddhsham@gmail.com>
2020-02-02 16:33:15 +0530

    Componentize dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -9,7 +9,7 @@
           <div class="box pad-top">
             <div class="level">
               <div class="level-left">
-            <h3 class="is-size-5 name">👋 Hi {{ this.$store.state.user.data.name }}! What would you like to do today?</h3>
+            <h3 class="is-size-5">👋 Hi <span class="name">{{ this.$store.state.user.data.name }}</span>! What would you like to do today?</h3>
               </div>
               <div class="level-right">
                 <div class="buttons">
@@ -19,6 +19,15 @@
               </div>
             </div>
           </div>
+
+          <div class="pad-top">
+            <Upcoming/>
+          </div>
+
+          <div class="pad-top">
+            <Resources/>
+          </div>
+
         </div>
       </div>
     </div>
@@ -27,13 +36,17 @@
 
 <script>
 import Sidemenu from '~/components/Sidemenu.vue';
+import Upcoming from '~/components/Upcoming.vue';
+import Resources from '~/components/Resources.vue';
 import { fireDb } from '@/services/firebase';
 
   export default {
     layout: 'student',
     middleware: 'auth',
     components: {
-      Sidemenu
+      Sidemenu,
+      Upcoming,
+      Resources
     }
   }
 
@@ -42,13 +55,21 @@ import { fireDb } from '@/services/firebase';
 <style>
 
 .dash {
-    height: 300vh;
+    /* height: 300vh; */
     overflow-x: scroll;
+    padding-bottom: 5rem;
 }
 .pad-top {
   margin-top: 2.5rem;
 }
+.pad-top-s {
+  margin-top: 1rem;
+}
 .name {
   text-transform: capitalize;
+}
+.sidenav {
+  padding-left: 0;
+  padding-right: 0;
 }
 </style>
